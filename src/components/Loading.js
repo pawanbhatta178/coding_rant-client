92c669a6685c950d149bcaaa4dcc6b62a600476e
pawan <pawanbhatta178@gmail.com>
2021-01-24 19:21:59 -0500

    border added

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,9 +1,13 @@
 import React from "react";
 import "./Loading.css";
-const Loading = ({ text }) => {
+const Loading = ({ text, border }) => {
   return (
     <div className="h-full w-full flex justify-center items-center ">
-      <div className="border p-1 border-purple-300 rounded-full h-16 w-16 flex flex-col gap-y-2 justify-center items-center shadow-lg ">
+      <div
+        className={` rounded-full h-16 w-16 flex flex-col gap-y-2 justify-center items-center ${
+          border && "border p-1 border-purple-300  shadow-lg"
+        }`}
+      >
         <div className="loader "></div>
         <div className="text-xs font-thin">{text}</div>
       </div>
